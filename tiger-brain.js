c6f89dd409c391039ac8e0ddc0e9b54c29f9380d
cybercafeservice24-boop <cybercafeservice24@gmail.com>
2026-02-13 18:57:30 +0530

    Brain Sync

---
+++ tiger-brain.js
@@ -0,0 +1,34 @@
+
+        var realMaal = [{"t": "Goddess Receives A Sunrise Surprise!", "i": "https://ei-ph.rdtcdn.com/videos/202012/07/377671551/thumbs_15/(m=e0YH8f)(mh=X8mJPMGAjC2MJC5c)9.jpg", "e": "https://embed.redtube.com/?id=42566911"}, {"t": "Public Agent Hot USA tourist college girl in Czech Republic asks for directions and ends up fucking a big dick outdoors", "i": "https://ei-ph.rdtcdn.com/videos/202407/18/455323171/original/(m=e0YH8f)(mh=a0iiVbZaz6piQpUF)6.jpg", "e": "https://embed.redtube.com/?id=189767871"}, {"t": "\"He Cheated On Me? Then I'll Fuck His Best Friend!\"", "i": "https://ei-ph.rdtcdn.com/videos/202406/28/454459061/original/(m=e0YH8f)(mh=l69IkWe9fMClPjN7)0.jpg", "e": "https://embed.redtube.com/?id=189804311"}, {"t": "Stepbro! Don't Cum Inside Me Or I'll Tell Our Parents!", "i": "https://ei-ph.rdtcdn.com/videos/202405/25/452943211/original/(m=e0YH8f)(mh=MpRT-jxbf864rKeM)0.jpg", "e": "https://embed.redtube.com/?id=189526311"}, {"t": "Sophia Locke in Step Mom Only Wears a Towel While We are on Vacation", "i": "https://ei-ph.rdtcdn.com/videos/202301/24/424043901/original/(m=e0YH8f)(mh=nRfJIGpnQG94_94T)0.jpg", "e": "https://embed.redtube.com/?id=43314271"}, {"t": "Ok Stepbro, Fuck Me But Dont Cum Inside Me Like Last Time", "i": "https://ei-ph.rdtcdn.com/videos/202409/20/458031571/original/(m=e0YH8f)(mh=3d48IXwuOmmjF-vu)0.jpg", "e": "https://embed.redtube.com/?id=190326151"}, {"t": "MOFOS - Chloe Bang Uses Her Natural Tits To Seduce Alex Legend Into Fucking Her Horny Pussy", "i": "https://ei-ph.rdtcdn.com/videos/202310/23/441696211/original/(m=e0YH8f)(mh=kcObjZxIszkBeh9w)0.jpg", "e": "https://embed.redtube.com/?id=103013281"}, {"t": "Ultra Sizzling Step-mother Vanessa Cage Will Do Anything To Continue Fucking Her Stepson - PervMom", "i": "https://ei-ph.rdtcdn.com/videos/202306/30/434559641/original/(m=e0YH8f)(mh=UFYJV1B03LSJ7ERY)0.jpg", "e": "https://embed.redtube.com/?id=102105141"}, {"t": "Step-Sis Uses Her Big Natural Boobs To Tittyfuck Me...", "i": "https://ei-ph.rdtcdn.com/videos/202310/25/441816251/original/(m=e0YH8f)(mh=y67hibD8JQZCxtNd)0.jpg", "e": "https://embed.redtube.com/?id=103165261"}, {"t": "Caught My Stepsis Masturbating And Fucked Her Pussy", "i": "https://ei-ph.rdtcdn.com/videos/202210/14/417524461/original/(m=e0YH8f)(mh=vwAD6R04cB11Ejfm)0.jpg", "e": "https://embed.redtube.com/?id=42756621"}, {"t": "Step Mom seduce Step Son and help him to cum quick while Dad is not home CarryLight MILF", "i": "https://ei-ph.rdtcdn.com/videos/202210/22/418042391/original/(m=e0YH8f)(mh=pdSREVQ9CBkr_zEd)0.jpg", "e": "https://embed.redtube.com/?id=42749521"}, {"t": "((WATCH THIS)) Stepmom Uses HUGE Ass To Pound Skinny Stepson!!  Jennifer Exxotic Fucks Young Guy", "i": "https://ei-ph.rdtcdn.com/videos/202209/26/416314061/thumbs_5/(m=e0YH8f)(mh=ckpBcNJ8fy7ZKysL)15.jpg", "e": "https://embed.redtube.com/?id=42584371"}, {"t": "Ani Black Fox in anal doggystyle fuck buddy scene", "i": "https://ei-ph.rdtcdn.com/videos/202412/02/461314491/original/(m=e0YH8f)(mh=eYW5AcTVQ9z0UAFd)0.jpg", "e": "https://embed.redtube.com/?id=190749101"}, {"t": "incredible fucking of my Brazilian friend complete on the jet ski Chris Diamond", "i": "https://ei-ph.rdtcdn.com/videos/202311/29/443861461/original/(m=e0YH8f)(mh=dmqDj8ZxEDwHAntA)8.jpg", "e": "https://embed.redtube.com/?id=103721901"}, {"t": "Step Mom seduce Step Son and help him to cum quick while is not home CarryLight MILF", "i": "https://ei-ph.rdtcdn.com/videos/202501/31/463918755/original/(m=e0YH8f)(mh=3zZiFbdRSyeT_xrm)0.jpg", "e": "https://embed.redtube.com/?id=190972971"}, {"t": "stepdaughter got stuck - daddy uses her to fuck her like a doll", "i": "https://ei-ph.rdtcdn.com/videos/202006/19/325054211/original/(m=e0YH8f)(mh=XvNIRZlZieMtUTvA)9.jpg", "e": "https://embed.redtube.com/?id=41745611"}, {"t": "Are You Done Yet?", "i": "https://ei-ph.rdtcdn.com/videos/202205/09/407790691/original/(m=e0YH8f)(mh=niTzK5_k2p9e_JiH)15.jpg", "e": "https://embed.redtube.com/?id=41557241"}, {"t": "Babydoll usa mi verga gigante - COFLA24CM", "i": "https://ei-ph.rdtcdn.com/videos/202403/28/450246741/thumbs_55/(m=e0YH8f)(mh=l0MNyDSeJL_TBXOC)11.jpg", "e": "https://embed.redtube.com/?id=188988001"}, {"t": "Cum Hungry Stepsister Begs To Cum In Her Mouth", "i": "https://ei-ph.rdtcdn.com/videos/202311/01/442236701/original/(m=e0YH8f)(mh=mWabs3GlenkXAr2Z)0.jpg", "e": "https://embed.redtube.com/?id=103107991"}, {"t": "\"Yes Honey I'm Alone In My Hotel Room\" Cheating GF Creampied", "i": "https://ei-ph.rdtcdn.com/videos/202303/29/428414121/original/(m=e0YH8f)(mh=hJgvBivm4MCvFCvV)0.jpg", "e": "https://embed.redtube.com/?id=43558251"}];
+        
+        // Purane dummy 'card' function ko asli data se override karna
+        window.card = function(clickAction, label, v) {
+            var item = v || {t: 'Loading...', i: '', e: '#'};
+            return `<div class='card' onclick="playTiger('${item.e}')">
+                <span class='hd-batch-gold'>1080p HD</span>
+                <div class='thumb'><img src='${item.i}' style='width:100%; aspect-ratio:16/9; object-fit:cover;'/></div>
+                <div style='padding:8px; font-size:11px; font-weight:bold; color:#fff; text-align:center;'>${item.t}</div>
+                <div class='t-stats'><span>👁️ ${Math.floor(Math.random()*900+100)}K</span><span>👍 ${Math.floor(Math.random()*20+80)}%</span></div>
+            </div>`;
+        };
+
+        // Purane 'renderTigerData' ko override karna
+        window.renderTigerData = function(tid) {
+            var el = document.getElementById(tid);
+            if(el) {
+                el.innerHTML = '';
+                realMaal.forEach(function(v) { el.innerHTML += window.card('', '', v); });
+            }
+        };
+
+        // Asli Player Link Injector
+        window.playTiger = function(link) {
+            document.getElementById('tiger-home-grid-area').style.display = 'none';
+            document.getElementById('tiger-player-area').style.display = 'block';
+            document.querySelector('.tiger-video-wrap iframe').src = link;
+            window.scrollTo({top: 0, behavior: 'smooth'});
+        };
+
+        // Page load hote hi dummy ko hatao aur asli dikhao
+        setTimeout(function() { window.renderTigerData('tiger-home-main-grid'); }, 500);
+        
